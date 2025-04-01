ca34281501855392b3a59ee828bda9dcb137d1d5
RND-HW3 <RND-HW3@RND-HW3>
2025-04-01 09:00:23 +0900

    초기 커밋.

---
+++ AnonymousType/MainApp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AnonymousType
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {   // 무명형식일때 선언방법
+            var a = new { Name = "kdw", Age = 25 };
+
+            Console.WriteLine($"Name: {a.Name}");
+            Console.WriteLine($"Age: {a.Age}");
+
+            Console.WriteLine();
+
+            var b = new { Subject = "수학", Scores = new int[] { 90, 80, 70, 60 } };
+            Console.WriteLine($"Subject: {b.Subject}");
+            foreach (var score in b.Scores)
+            {
+                Console.WriteLine($"Score: {score}");
+            }                                   
+        }
+    }
+}
